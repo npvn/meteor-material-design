a14f997e0ca954184b10ea10a7eaa1b83263bf40
Phuc Nguyen <mail@nguyenphuc.info>
2014-09-14 16:34:19 +0700

    Initial commit

---
+++ js/material.js
@@ -0,0 +1,38 @@
+/* globals ripples */
+
+$(function (){
+
+    ripples.init(".btn:not(.btn-link), .navbar a, .nav-tabs a, .withripple");
+
+    // Add fake-checkbox to material checkboxes
+    $(".checkbox > label > input").after("<span class=ripple></span><span class=check></span>");
+
+    // Add fake-radio to material radios
+    $(".radio > label > input").after("<span class=ripple></span><span class=circle></span><span class=check></span>");
+
+    // Add elements for material inputs
+    $("input.form-control, textarea.form-control, select.form-control").each( function() {
+        $(this).wrap("<div class=form-control-wrapper></div>");
+        $(this).after("<span class=material-input></span>");
+        if ($(this).hasClass("floating-label")) {
+            var placeholder = $(this).attr("placeholder");
+            $(this).attr("placeholder", null).removeClass("floating-label");
+            $(this).after("<div class=floating-label>" + placeholder + "</div>");
+        }
+        if ($(this).val() === "") {
+            $(this).addClass("empty");
+        }
+    });
+
+    $(document).on("keyup change", ".form-control", function() {
+        if ($(this).val() !== "") {
+            $(this).removeClass("empty");
+        } else {
+            $(this).addClass("empty");
+        }
+    });
+    $(document).on("keydown", ".form-control", function() {
+        $(this).removeClass("empty");
+    });
+});
+
---
+++ package.js
@@ -0,0 +1,36 @@
+Package.describe({
+    summary: 'A foundation for implementing Material Design in Meteor'
+    , version: '0.0.1'
+});
+
+Package.onUse(function(api) {
+    api.use('jquery', 'client');
+    api.use('less', 'client');
+
+    api.addFiles('bootstrap/bootstrap.min.css', 'client');
+    api.addFiles('bootstrap/bootstrap.min.js', 'client');
+
+    api.addFiles('less/variables.lessimport', 'client');
+    api.addFiles('less/mixins.lessimport', 'client');
+    api.addFiles('less/prefixer.lessimport', 'client');
+    api.addFiles('less/animations.lessimport', 'client');
+    api.addFiles('less/shadows.lessimport', 'client');
+    api.addFiles('less/alerts.lessimport', 'client');
+    api.addFiles('less/buttons.lessimport', 'client');
+    api.addFiles('less/checkboxes.lessimport', 'client');
+    api.addFiles('less/icons.lessimport', 'client');
+    api.addFiles('less/inputs.lessimport', 'client');
+    api.addFiles('less/lists.lessimport', 'client');
+    api.addFiles('less/navbar.lessimport', 'client');
+    api.addFiles('less/plugin-nouislider.lessimport', 'client');
+    api.addFiles('less/plugin-snackbarjs.lessimport', 'client');
+    api.addFiles('less/popups.lessimport', 'client');
+    api.addFiles('less/progress.lessimport', 'client');
+    api.addFiles('less/radios.lessimport', 'client');
+    api.addFiles('less/ripples.lessimport', 'client');
+    api.addFiles('less/tabs.lessimport', 'client');
+    api.addFiles('less/material.less', 'client');
+
+    api.addFiles('js/ripples.js', 'client');
+    api.addFiles('js/material.js', 'client');
+});
